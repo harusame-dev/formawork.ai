c347284c354e6860ed2d168ebcc9cc806e89d6e6
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-12-09 22:34:52 +0900

    キューの名前サイズオーバー対応

--- packages/db/src/queue-names.ts
+++ packages/db/src/queue-names.ts
@@ -1,3 +1,3 @@
 import { schemaName } from "./pgschema";
 
-export const ADVICE_QUEUE_NAME = `${schemaName}_customer_note_advice_generation`;
+export const ADVICE_QUEUE_NAME = `${schemaName}_service_advice`.slice(0, 48);
